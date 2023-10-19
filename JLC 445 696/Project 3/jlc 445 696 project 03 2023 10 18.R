f91d354a29c8874c9853bdb1306a64129468e698
matthew d'anna <46403937+matthew-danna@users.noreply.github.com>
2023-10-19 09:07:44 -0400

    Update jlc 445 696 project 03 2023 10 18.R

--- JLC 445 696/Project 3/jlc 445 696 project 03 2023 10 18.R
+++ JLC 445 696/Project 3/jlc 445 696 project 03 2023 10 18.R
@@ -229,7 +229,8 @@ ggplot(forecasts.day, aes(DATE, TODAY)) +
     x = "Date",
     y = "Expected Crime Count"
   ) +
-  theme(axis.text.x= element_text(angle = 90))
+  theme(axis.text.x= element_text(angle = 90)) +
+  scale_x_continuous("Date", labels = as.character(forecasts.day$DATE), breaks = forecasts.day$DATE)
 
 ### HOTSPOT MAP
 dc.roads <- roads("DC", "District of Columbia")
